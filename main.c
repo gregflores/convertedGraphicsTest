f2f2a39687ae26aaa43f92ac5c7c0e67ea53e446
Gregorio Flores <floresgreg@hotmail.com>
2017-02-25 13:05:54 -0600

    Updated LCD to have demo PWR lines

--- main.c
+++ main.c
@@ -50,8 +50,8 @@ void main(void) {
 
 	drawString(4, 2, FONT_MD, "MEGAHERTZ");
 	drawString(236, 2, FONT_MD, "GIRAF Tech");
+
 	drawString(4, 34, FONT_MD, " 1");
-	//setColor(COLOR_16_WHITE);
 	drawString(4, 50, FONT_MD, " 2");
 	drawString(4, 66, FONT_MD, " 3");
 	drawString(4, 82, FONT_MD, " 4");
@@ -61,22 +61,50 @@ void main(void) {
 	drawString(4, 146, FONT_MD, " 8");
 	drawString(4, 162, FONT_MD, " 9");
 	drawString(4, 178, FONT_MD, "10");
+
 	drawString(4, 210, FONT_MD, "TEMP  Inside:");
 	drawString(116, 210, FONT_MD, "000F");
 	drawString(4, 226, FONT_MD, "TEMP Outside:");
 	drawString(116, 226, FONT_MD, "000F");
+
 	drawString(156, 210, FONT_MD, " IP: 255.255.255.255");
 	drawString(156, 226, FONT_MD, "Web: GIRAFTECH.XYZ");
-	//drawString(20, 34, FONT_MD, "W FWD");
 
-	//drawTILogo(30, 82, COLOR_16_BLUE);
-	//drawTILogo(32, 80, COLOR_16_RED);
-	//drawTILogo(31, 81, COLOR_16_RED);
-	//drawTILogo(30, 80, COLOR_16_RED);
-	//drawTILogo(32, 82, COLOR_16_WHITE);
-	//drawPixel(50,60);
+	drawString(28, 34, FONT_MD, "FWD: 000W RVS: 000W INACTIVE");
+	drawString(28, 50, FONT_MD, "FWD: 000W RVS: 000W INACTIVE");
+	drawString(28, 66, FONT_MD, "FWD: 000W RVS: 000W INACTIVE");
+	drawString(28, 82, FONT_MD, "FWD: 000W RVS: 000W INACTIVE");
+	drawString(28, 98, FONT_MD, "FWD: 000W RVS: 000W INACTIVE");
+	drawString(28, 114, FONT_MD, "FWD: 000W RVS: 000W INACTIVE");
+	drawString(28, 130, FONT_MD, "FWD: 000W RVS: 000W INACTIVE");
+	drawString(28, 146, FONT_MD, "FWD: 000W RVS: 000W INACTIVE");
+	drawString(28, 162, FONT_MD, "FWD: 000W RVS: 000W INACTIVE");
+	drawString(28, 178, FONT_MD, "FWD: 000W RVS: 000W INACTIVE");
+
+	setColor(COLOR_16_RED);
+	fillCircle(263, 39, 4);
+	fillCircle(263, 55, 4);
+	fillCircle(263, 71, 4);
+	fillCircle(263, 87, 4);
+	fillCircle(263, 103, 4);
+	fillCircle(263, 119, 4);
+	fillCircle(263, 135, 4);
+	fillCircle(263, 151, 4);
+	fillCircle(263, 167, 4);
+	fillCircle(263, 183, 4);
+	setColor(COLOR_16_WHITE);
+
 	while (1) {
-		//shesGotColors(50);
+		drawString(68, 34, FONT_MD, "FWD: 000W RVS: 000W INACTIVE");
+		drawString(68, 50, FONT_MD, "FWD: 000W RVS: 000W INACTIVE");
+		drawString(68, 66, FONT_MD, "FWD: 000W RVS: 000W INACTIVE");
+		drawString(68, 82, FONT_MD, "FWD: 000W RVS: 000W INACTIVE");
+		drawString(68, 98, FONT_MD, "FWD: 000W RVS: 000W INACTIVE");
+		drawString(68, 114, FONT_MD, "FWD: 000W RVS: 000W INACTIVE");
+		drawString(68, 130, FONT_MD, "FWD: 000W RVS: 000W INACTIVE");
+		drawString(68, 146, FONT_MD, "FWD: 000W RVS: 000W INACTIVE");
+		drawString(68, 162, FONT_MD, "FWD: 000W RVS: 000W INACTIVE");
+		drawString(68, 178, FONT_MD, "FWD: 000W RVS: 000W INACTIVE");
 	}
 
 }
