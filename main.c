189b3031487d504551eab30b383b6fcb7129b809
Gregorio Flores <floresgreg@hotmail.com>
2017-02-25 18:50:44 -0600

    Full LCD write test.
    The LCD cycles every value that can be changed by the program.

--- main.c
+++ main.c
@@ -68,48 +68,161 @@ void main(void) {
 	drawString(156, 210, FONT_MD, " IP: 255.255.255.255");
 	drawString(156, 226, FONT_MD, "Web: GIRAFTECH.XYZ");
 
-	drawString(28, 34, FONT_MD, "FWD: 132W RVS: 000W INACTIVE");
-	drawString(28, 50, FONT_MD, "FWD: 000W RVS: 000W INACTIVE");
-	drawString(28, 66, FONT_MD, "FWD: 000W RVS: 000W INACTIVE");
-	drawString(28, 82, FONT_MD, "FWD: 000W RVS: 000W INACTIVE");
-	drawString(28, 98, FONT_MD, "FWD: 000W RVS: 000W INACTIVE");
-	drawString(28, 114, FONT_MD, "FWD: 000W RVS: 000W INACTIVE");
-	drawString(28, 130, FONT_MD, "FWD: 000W RVS: 000W INACTIVE");
-	drawString(28, 146, FONT_MD, "FWD: 000W RVS: 000W INACTIVE");
-	drawString(28, 162, FONT_MD, "FWD: 000W RVS: 000W INACTIVE");
-	drawString(28, 178, FONT_MD, "FWD: 000W RVS: 000W INACTIVE");
-
-	setColor(COLOR_16_RED);
-	fillCircle(263, 39, 4);
-	fillCircle(263, 55, 4);
-	fillCircle(263, 71, 4);
-	fillCircle(263, 87, 4);
-	fillCircle(263, 103, 4);
-	fillCircle(263, 119, 4);
-	fillCircle(263, 135, 4);
-	fillCircle(263, 151, 4);
-	fillCircle(263, 167, 4);
-	fillCircle(263, 183, 4);
-	setColor(COLOR_16_WHITE);
+	drawString(28, 34, FONT_MD, "FWD: 000W RVS: 000W");
+	drawString(28, 50, FONT_MD, "FWD: 000W RVS: 000W");
+	drawString(28, 66, FONT_MD, "FWD: 000W RVS: 000W");
+	drawString(28, 82, FONT_MD, "FWD: 000W RVS: 000W");
+	drawString(28, 98, FONT_MD, "FWD: 000W RVS: 000W");
+	drawString(28, 114, FONT_MD, "FWD: 000W RVS: 000W");
+	drawString(28, 130, FONT_MD, "FWD: 000W RVS: 000W");
+	drawString(28, 146, FONT_MD, "FWD: 000W RVS: 000W");
+	drawString(28, 162, FONT_MD, "FWD: 000W RVS: 000W");
+	drawString(28, 178, FONT_MD, "FWD: 000W RVS: 000W");
 
 	uint16_t fwd[10];
-	//uint16_t rvs[10];
-	char fwd_temp[] = "00";
-	//int8_t rvs_temp[3];
+	uint16_t rvs[10];
+	int8_t fwd_temp[] = "";
+	int8_t rvs_temp[] = "";
 	uint16_t i, j;
-
+	int16_t activity = 0;
+	int16_t insideTemp, outsideTemp;
+	int8_t insideTempC[] = "", outsideTempC[] = "";
+	int16_t ipAddress[4] = {255, 255, 255, 255};
+	int8_t ipAddress_temp[] = "";
+	/* === while === */
 	while (1)
 	{
+
+		/*switch*/
+		switch(activity)
+		{
+		case 0:
+			drawString(188, 34, FONT_MD_BKG, "INACTIVE");
+			drawString(188, 50, FONT_MD_BKG, "INACTIVE");
+			drawString(188, 66, FONT_MD_BKG, "INACTIVE");
+			drawString(188, 82, FONT_MD_BKG, "INACTIVE");
+			drawString(188, 98, FONT_MD_BKG, "INACTIVE");
+			drawString(188, 114, FONT_MD_BKG, "INACTIVE");
+			drawString(188, 130, FONT_MD_BKG, "INACTIVE");
+			drawString(188, 146, FONT_MD_BKG, "INACTIVE");
+			drawString(188, 162, FONT_MD_BKG, "INACTIVE");
+			drawString(188, 178, FONT_MD_BKG, "INACTIVE");
+			setColor(COLOR_16_RED);
+			fillCircle(263, 39, 4);
+			fillCircle(263, 55, 4);
+			fillCircle(263, 71, 4);
+			fillCircle(263, 87, 4);
+			fillCircle(263, 103, 4);
+			fillCircle(263, 119, 4);
+			fillCircle(263, 135, 4);
+			fillCircle(263, 151, 4);
+			fillCircle(263, 167, 4);
+			fillCircle(263, 183, 4);
+			setColor(COLOR_16_WHITE);
+			activity = 1;
+			break;
+		case 1:
+			drawString(188, 34, FONT_MD_BKG, "  ACTIVE");
+			drawString(188, 50, FONT_MD_BKG, "  ACTIVE");
+			drawString(188, 66, FONT_MD_BKG, "  ACTIVE");
+			drawString(188, 82, FONT_MD_BKG, "  ACTIVE");
+			drawString(188, 98, FONT_MD_BKG, "  ACTIVE");
+			drawString(188, 114, FONT_MD_BKG, "  ACTIVE");
+			drawString(188, 130, FONT_MD_BKG, "  ACTIVE");
+			drawString(188, 146, FONT_MD_BKG, "  ACTIVE");
+			drawString(188, 162, FONT_MD_BKG, "  ACTIVE");
+			drawString(188, 178, FONT_MD_BKG, "  ACTIVE");
+			setColor(COLOR_16_GREEN);
+			fillCircle(263, 39, 4);
+			fillCircle(263, 55, 4);
+			fillCircle(263, 71, 4);
+			fillCircle(263, 87, 4);
+			fillCircle(263, 103, 4);
+			fillCircle(263, 119, 4);
+			fillCircle(263, 135, 4);
+			fillCircle(263, 151, 4);
+			fillCircle(263, 167, 4);
+			fillCircle(263, 183, 4);
+			setColor(COLOR_16_WHITE);
+			activity = 0;
+			break;
+		default:
+			break;
+		} /*switch*/
+
 		for (i = 0; i < 256; i++)
 		{
 			for (j = 0; j < 10; j++)
 				{
 					fwd[j] = i;
+					rvs[j] = 255-i;
 				}
-			sprintf(fwd_temp, "%.3u", fwd[1]);
-			drawString(68, 34, FONT_MD_BKG, (int8_t*)fwd_temp);
-			_delay_cycles(10000000);
+
+			ipAddress[0] = i;
+			ipAddress[1] = 255-i;
+			ipAddress[2] = i;
+			ipAddress[3] = 255-i;
+			sprintf((char*)ipAddress_temp, "%.3u", ipAddress[0]);
+			drawString(196, 210, FONT_MD_BKG, ipAddress_temp);
+			sprintf((char*)ipAddress_temp, "%.3u", ipAddress[1]);
+			drawString(228, 210, FONT_MD_BKG, ipAddress_temp);
+			sprintf((char*)ipAddress_temp, "%.3u", ipAddress[2]);
+			drawString(260, 210, FONT_MD_BKG, ipAddress_temp);
+			sprintf((char*)ipAddress_temp, "%.3u", ipAddress[3]);
+			drawString(292, 210, FONT_MD_BKG, ipAddress_temp);
+
+			insideTemp = i;
+			outsideTemp = 255-i;
+			sprintf((char*)insideTempC, "%.3u", insideTemp);
+			drawString(116, 210, FONT_MD_BKG, insideTempC);
+			sprintf((char*)outsideTempC, "%.3u", outsideTemp);
+			drawString(116, 226, FONT_MD_BKG, outsideTempC);
+
+
+			sprintf((char*)fwd_temp, "%.3u", fwd[0]);
+			drawString(68, 34, FONT_MD_BKG, fwd_temp);
+			sprintf((char*)fwd_temp, "%.3u", fwd[1]);
+			drawString(68, 50, FONT_MD_BKG, fwd_temp);
+			sprintf((char*)fwd_temp, "%.3u", fwd[2]);
+			drawString(68, 66, FONT_MD_BKG, fwd_temp);
+			sprintf((char*)fwd_temp, "%.3u", fwd[3]);
+			drawString(68, 82, FONT_MD_BKG, fwd_temp);
+			sprintf((char*)fwd_temp, "%.3u", fwd[4]);
+			drawString(68, 98, FONT_MD_BKG, fwd_temp);
+			sprintf((char*)fwd_temp, "%.3u", fwd[5]);
+			drawString(68, 114, FONT_MD_BKG, fwd_temp);
+			sprintf((char*)fwd_temp, "%.3u", fwd[6]);
+			drawString(68, 130, FONT_MD_BKG, fwd_temp);
+			sprintf((char*)fwd_temp, "%.3u", fwd[7]);
+			drawString(68, 146, FONT_MD_BKG, fwd_temp);
+			sprintf((char*)fwd_temp, "%.3u", fwd[8]);
+			drawString(68, 162, FONT_MD_BKG, fwd_temp);
+			sprintf((char*)fwd_temp, "%.3u", fwd[9]);
+			drawString(68, 178, FONT_MD_BKG, fwd_temp);
+
+			sprintf((char*)rvs_temp, "%.3u", rvs[0]);
+			drawString(148, 34, FONT_MD_BKG, rvs_temp);
+			sprintf((char*)rvs_temp, "%.3u", rvs[1]);
+			drawString(148, 50, FONT_MD_BKG, rvs_temp);
+			sprintf((char*)rvs_temp, "%.3u", rvs[2]);
+			drawString(148, 66, FONT_MD_BKG, rvs_temp);
+			sprintf((char*)rvs_temp, "%.3u", rvs[3]);
+			drawString(148, 82, FONT_MD_BKG, rvs_temp);
+			sprintf((char*)rvs_temp, "%.3u", rvs[4]);
+			drawString(148, 98, FONT_MD_BKG, rvs_temp);
+			sprintf((char*)rvs_temp, "%.3u", rvs[5]);
+			drawString(148, 114, FONT_MD_BKG, rvs_temp);
+			sprintf((char*)rvs_temp, "%.3u", rvs[6]);
+			drawString(148, 130, FONT_MD_BKG, rvs_temp);
+			sprintf((char*)rvs_temp, "%.3u", rvs[7]);
+			drawString(148, 146, FONT_MD_BKG, rvs_temp);
+			sprintf((char*)rvs_temp, "%.3u", rvs[8]);
+			drawString(148, 162, FONT_MD_BKG, rvs_temp);
+			sprintf((char*)rvs_temp, "%.3u", rvs[9]);
+			drawString(148, 178, FONT_MD_BKG, rvs_temp);
 		}
-	}
+
+
+	}/* === while === */
 }
 
